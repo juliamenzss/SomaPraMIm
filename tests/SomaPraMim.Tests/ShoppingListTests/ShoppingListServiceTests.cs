789039d41ae5c7eefb3a2eca7050acb34b42dfb9
Júlia <juliamenzss@gmail.com>
2025-03-10 22:09:24 -0300

    feat: adiciona teste de getTotal

--- tests/SomaPraMim.Tests/ShoppingListTests/ShoppingListServiceTests.cs
+++ tests/SomaPraMim.Tests/ShoppingListTests/ShoppingListServiceTests.cs
@@ -1,40 +1,43 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Xunit;
 using FluentAssertions;
+using SomaPraMim.Domain.Contexts;
+using Moq;
+using Moq.EntityFrameworkCore;
+using SomaPraMim.Domain.Entities;
+using SomaPraMim.Application.Services.ShoppingListServices;
 
-
-namespace tests.SomaPraMim.Test.ShoppingList
+namespace SomaPraMim.Tests.ShoppingListTests
 {
     public class ShoppingListServiceTests
     {
         private readonly Mock<IShoppingListContext> _context;
-        private readonly UserService _service;
+        private readonly ShoppingListService _service;
 
         public ShoppingListServiceTests(){
             _context = new Mock<IShoppingListContext>();
-            _service = new UserService(_context.Object);
+            _service = new ShoppingListService(_context.Object);
         }
 
         [Fact(DisplayName = "001 - Retorna valor total")]
-        public async GivenShoppingList_WhenGetTotal_ThenReturnsCorrectSum()
+        public async Task GivenShoppingListWhenGetTotalThenReturnsCorrectSum()
         {
             var shoppingListId = Random.Shared.Next(0, 100);
 
-            var fakeShoppingLIst = new List<ShoppingList>{
+            var fakeShoppingList = new List<ShoppingItem>
+            {
                 new () {ShoppingListId = shoppingListId, Price = 15m, Quantity = 2},
                 new () {ShoppingListId = shoppingListId, Price = 2.5m, Quantity = 4},
             };
 
             _context
                 .Setup(x => x.ShoppingItems)
-                .ReturnDbSet(fakeShoppingLIst);
+                .ReturnsDbSet(fakeShoppingList);
 
             var expectResult = await _service.GetShoppingListTotal(shoppingListId);
 
             expectResult.Should().Be(40m);
         }
+
+
+        
     }
 }
