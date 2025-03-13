b87cc4116f691baadf820e810c056c69426c3764
Júlia <juliamenzss@gmail.com>
2025-03-13 16:09:19 -0300

    feat: adiciona shopping item controller

---
+++ src/Backend/SomaPraMim.API/Controllers/ShoppingItemController.cs
@@ -0,0 +1,73 @@
+using System.Data.Common;
+using Microsoft.AspNetCore.Mvc;
+using SomaPraMim.Application.Services.ShoppingItemServices;
+using SomaPraMim.Communication.Requests.ShoppingItemRequests;
+using SomaPraMim.Communication.Responses;
+
+namespace SomaPraMim.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ShoppingItemController(IShoppingItemService service) : ControllerBase
+    {
+        private readonly IShoppingItemService _service = service ?? throw new ArgumentNullException(nameof(service));
+
+
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [HttpPost]
+        public async Task<IActionResult> Create(ShoppingItemCreateRequest request)
+        {
+            try
+            {
+                if (!ModelState.IsValid) return BadRequest(ModelState);
+
+                var shoppingItem = await _service.CreateShoppingItem(request);
+                return CreatedAtAction(nameof(GetById), new { id = shoppingItem.Id }, shoppingItem);
+            }
+            catch (DbException)
+            {
+                return BadRequest();
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(long id)
+        {
+            var item = await _service.GetShoppingItemById(id);
+            if (item == null) return NotFound();
+
+            return Ok(item);
+        }
+
+
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<ShoppingItemResponse>))]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [HttpGet]
+        public async Task<IActionResult> GetAll(int page = 1, int pagesize = 10)
+        {
+            var shoppingItems = await _service.GetShoppingItem(page, pagesize);
+
+            if (shoppingItems == null || !shoppingItems.Any())
+            {
+                return NotFound();
+            }
+
+            var result = new PaginateResponse<ShoppingItemResponse>
+            {
+                CurrentPage = page,
+                PageSize = pagesize,
+                TotalItems = await _service.GetTotal(),
+                Items = shoppingItems,
+            };
+            return Ok(result);
+        }
+
+    }
+}
