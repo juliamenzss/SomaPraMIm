6208bafa1837c21d37515eb7ef88e5e2fdc3991c
Júlia <juliamenzss@gmail.com>
2025-03-13 16:15:27 -0300

    refactor: refatora update request

--- src/Shared/SomaPraMim.Communication/Requests/UserRequests/UserUpdateRequest.cs
+++ src/Shared/SomaPraMim.Communication/Requests/UserRequests/UserUpdateRequest.cs
@@ -3,7 +3,6 @@ namespace SomaPraMim.Communication.Requests.UserRequests
     public class UserUpdateRequest
     {
         public string Name { get; set; } = string.Empty;
-        public string Email { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
     }
 }
